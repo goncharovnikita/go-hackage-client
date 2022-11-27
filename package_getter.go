778e63bdd98dcfa64614fd56a804bc6089335fb5
Nikita Goncharov <goncharovnikita.dev@gmail.com>
2022-11-27 11:43:26 +0300

    add package getter

---
+++ package_getter.go
@@ -0,0 +1,48 @@
+package gohackageclient
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type packageGetter struct {
+	baseURL    string
+	httpClient *http.Client
+}
+
+func NewPackageGetter() *packageGetter {
+	return &packageGetter{
+		baseURL:    "https://hackage.haskell.org",
+		httpClient: http.DefaultClient,
+	}
+}
+
+func (c *packageGetter) GetPackageVersions(
+	ctx context.Context,
+	name string,
+) (PackageVersions, error) {
+	r := make(PackageVersions)
+
+	url := fmt.Sprintf("%s/package/%s.json", c.baseURL, name)
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode != 200 {
+		return nil, fmt.Errorf("non-ok response: %s", res.Status)
+	}
+
+	if err := json.NewDecoder(res.Body).Decode(&r); err != nil {
+		return nil, err
+	}
+
+	return r, nil
+}
